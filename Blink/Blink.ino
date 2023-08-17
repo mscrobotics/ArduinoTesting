c79980ba333333034f61ca9c413ae3a7ffe36b1e
msc_intra <109829955+mscrobotics@users.noreply.github.com>
2023-08-17 15:12:25 -0800

    create blink faster

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -10,4 +10,8 @@ void loop() {
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(2000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(200);                       // wait for a second
 }
